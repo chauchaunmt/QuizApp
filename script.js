0cb291a8bcacc855b7a4ffc59f01baa940aac058
chauchaunmt <chauchaunmt0808@gmail.com>
2020-10-31 12:32:31 +0700

    add quiz data

---
+++ script.js
@@ -0,0 +1,59 @@
+const quizData = [
+  {
+    question: 'Tuấn xoá đi 1 trong 10 số nguyên dương liên tiếp. Tổng của 9 số còn lại bằng 2006. Hỏi Tuấn đã xoá đi số nào?',
+    A: '218',
+    B: '219',
+    C: '220',
+    D: '225',
+    E: '227',
+    correct: 'B'
+  }, {
+    question: 'Nếu trung bình cộng của 5 số nguyên liên tiếp bằng 12 thì tổng của số nhỏ nhất và số lớn nhất bằng bao nhiêu?',
+    A: '24',
+    B: '14',
+    C: '12',
+    D: '11',
+    E: '10',
+    correct: 'A'
+  }, {
+    question: 'Nếu a, b, c là các số thực dương thì a + 1/(b +1/c) bằng:',
+    A: '(a + b)/c',
+    B: '(ac + bc + 1)/c',
+    C: '(abc + b + c)/c',
+    D: '(a + b + c)/(abc + 1)',
+    E: '(abc + b + c)/(abc + 1)',
+    correct: 'E'
+  }, {
+    question: 'Hùng nói về tủ sách của mình: “Đúng 25% số sách trong tủ sách của tôi là tiểu thuyết và đúng 1/9 số sách là thơ”. Nếu Hùng có khoảng từ 50 đến 100 cuốn sách thì hỏi Hùng có chính xác bao nhiêu cuốn sách?',
+    A: '50',
+    B: '54',
+    C: '64',
+    D: '72',
+    E: '93',
+    correct: 'D'
+  }, {
+    question: 'Nếu x, y, z là các số nguyên dương phân biệt nhỏ hơn 10 thì đại lượng (x-y)/z có thể nhận giá trị lớn nhất bằng',
+    A: '4',
+    B: '5',
+    C: '6',
+    D: '7',
+    E: '8',
+    correct: 'D'
+  }, {
+    question: 'Nếu 45% của n là 405 thì 35% của n là?',
+    A: '61',
+    B: '64',
+    C: '142',
+    D: '250',
+    E: '315',
+    correct: 'E'
+  }, {
+    question: 'Chiếc đồng hồ nhắc việc cứ 15 phút lại reo 1 lần. Nếu đồng hồ đã reo vào lúc 2:40 thì thời điểm nào dưới đây là thời điểm mà đồng hồ có thể reo?',
+    A: '4:05',
+    B: '5:30',
+    C: '6:45',
+    D: '7:15',
+    E: '8:10',
+    correct: 'E'
+  }
+]
