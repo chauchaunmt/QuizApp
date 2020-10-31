7f06fca2133d31d8f62ab231e396f7dd499aa3e7
chauchaunmt <chauchaunmt0808@gmail.com>
2020-10-31 21:40:33 +0700

    basic quiz

--- script.js
+++ script.js
@@ -72,4 +72,60 @@ const quizData = [
     E: 'Sự can thiệp của chính phủ không thể ảnh hưởng đến tỷ lệ lạm phát ở bất kỳ mức độ đáng kể nào.',
     correct: 'B'
   }
-]
+];
+
+const questionEl = document.getElementById('question');
+const answerEls = document.querySelectorAll(".answer");
+const quiz = document.getElementById('quiz');
+
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const e_text = document.getElementById('e_text');
+const submitBtn = document.getElementById('submit');
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+  deselectAnswer();
+  const currentQuizData = quizData[currentQuiz];
+  questionEl.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.A;
+  b_text.innerText = currentQuizData.B;
+  c_text.innerText = currentQuizData.C;
+  d_text.innerText = currentQuizData.D;
+  e_text.innerText = currentQuizData.E;
+}
+
+function getSelected() {
+  let answer = undefined;
+  answerEls.forEach((answerEl) => {
+    if(answerEl.checked)
+      answer = answerEl.id;
+  });
+  return answer;
+}
+
+function deselectAnswer() {
+  answerEls.forEach((answerEl) => {
+    answerEl.checked = false;
+  })
+}
+
+submitBtn.addEventListener("click", () => {
+  const answer = getSelected();
+  if(answer) {
+    if(answer === quizData[currentQuiz].correct)
+      score++;
+    currentQuiz++;
+    if(currentQuiz < quizData.length)
+      loadQuiz();
+    else
+      quiz.innerHTML = `<h2>Bạn trả lời ${score}/${quizData.length} câu đúng!</h2><button onclick="location.reload()">Thoát</button>`;
+  }
+
+})
